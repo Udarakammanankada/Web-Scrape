bc4a91fe88cfecbcc6ee4d6207803fda6cde78a3
UdaraKammanankada <udarakaushalya55@gmail.com>
2023-02-24 21:42:31 +0530

    Commit 1

--- src/test/java/WebScraper.java
+++ src/test/java/WebScraper.java
@@ -9,30 +9,27 @@ import java.io.IOException;
 public class WebScraper {
     public static void main(String[] args) {
         // Set the path to the chromedriver executable file
-        System.setProperty("webdriver.chrome.driver", "/home/vetstoria/udara/Udara/webdriver/chromedriver_linux64/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "add the path to chromedriver exe");
         // Create ChromeOptions object
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
-
-
         // Create a new instance of the Chrome driver
         WebDriver driver = new ChromeDriver(options);
-
         // Navigate to the website you want to scrape
         driver.get("https://en.wikipedia.org/wiki/Sri_Lanka");
-
         // Find and print the page title
         String pageTitle = driver.getTitle();
         System.out.println("Page Title: " + pageTitle);
-
-        // Find and print the page header
+        // Fine the web element
         WebElement contentElement = driver.findElement(By.id("mw-content-text"));
+        // Pass the content to string file and print it
         String content = contentElement.getText();
         System.out.println("content is: " + content);
 
+        // Write the content to a text file
 
         try {
-            FileWriter writer = new FileWriter("/home/vetstoria/udara/Udara/Restassured write/file2.txt");
+            FileWriter writer = new FileWriter("Add the path of the text file");
             writer.write(content);
             writer.close();
             System.out.println("Successfully wrote to the file.");
